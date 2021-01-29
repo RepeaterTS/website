3dbd84e6df93dd82afff599e482218dfc77bd35c
Jordan Jones <jordpjones@gmail.com>
2021-01-29 13:14:18 -0800

    default tag

--- src/data/MainSource.js
+++ src/data/MainSource.js
@@ -7,7 +7,7 @@ export default new DocsSource({
   name: 'Main library',
   global: 'Lever',
   repo: 'repeaterts/lever',
-  defaultTag: 'stable',
+  defaultTag: 'master',
   branchFilter: branch => !branchBlacklist.has(branch) && !branch.startsWith('dependabot/'),
   tagFilter: tag => semver.gte(tag, '9.0.0'),
 });
