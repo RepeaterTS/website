b2c48491d87da09831856926c7622bc9a609dce2
Kashalls <Kashalls@users.noreply.github.com>
2021-01-29 21:07:20 +0000

    Site build: 654ab541f861862aaa4df3d38fd3ef4ef94b64e4

---
+++ precache-manifest.0832ef15f9f2217798a1d29f0527085b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b398f78b0dacc89513c",
+    "url": "/css/app.a1dbaccf.css"
+  },
+  {
+    "revision": "fba71fa7d3d958719978528a885db80f",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "42e57baaf08d72d206976c7eaf7610d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b398f78b0dacc89513c",
+    "url": "/js/app.c11d79de.js"
+  },
+  {
+    "revision": "9ddd2a652437dadcab54",
+    "url": "/js/chunk-vendors.98987d00.js"
+  },
+  {
+    "revision": "6170cf74702a9e2bdc604c02396884ab",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c281316ea4aa526c0a1b2e0c13ea26a4",
+    "url": "/static/icons/android-chrome-144x144.png"
+  },
+  {
+    "revision": "e50cee0cb5f8d22817ae025c85c719e3",
+    "url": "/static/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "60c9fc2bf0b20295ced2875ff94cff5d",
+    "url": "/static/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "ad68b4696e0c7bf3a2898db0d76f644a",
+    "url": "/static/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "fba71fa7d3d958719978528a885db80f",
+    "url": "/static/icons/favicon.ico"
+  },
+  {
+    "revision": "4527cf161a113b682192809adad36f79",
+    "url": "/static/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "81b0dc7f875558618f0e4fb9acf7fa32",
+    "url": "/static/icons/safari-pinned-tab.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.d50a4d8b41125dbf88998a4abf4ad580.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/precache-manifest.0832ef15f9f2217798a1d29f0527085b.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /* global workbox */
 
