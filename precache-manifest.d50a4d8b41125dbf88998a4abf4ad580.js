a5d7d82cf94cbc2630b3b0090106432a547d3559
Kashalls <Kashalls@users.noreply.github.com>
2021-01-29 21:03:10 +0000

    Site build: 29f4ecf4e1e5c26417b3c17dae31d50952d1820f

---
+++ precache-manifest.d50a4d8b41125dbf88998a4abf4ad580.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecd9a7ccfc03e55394d2",
+    "url": "/css/app.a1dbaccf.css"
+  },
+  {
+    "revision": "93e0944caa85e50cb28d57877b2df83d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecd9a7ccfc03e55394d2",
+    "url": "/js/app.ba3dec15.js"
+  },
+  {
+    "revision": "9ddd2a652437dadcab54",
+    "url": "/js/chunk-vendors.98987d00.js"
+  },
+  {
+    "revision": "6170cf74702a9e2bdc604c02396884ab",
+    "url": "/manifest.json"
+  }
+]);
